c97e5ce3f02ffd6102443406ec517ea9068bd750
Samuel Cho <samuel.ms.cho@gmail.com>
2021-03-09 19:43:06 -0800

    Changed moveCar function conditions to data model direction property. Added condition for west to subtract from offsetX. #5

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 var $car = document.querySelector('.race-car');
 var offsetX = 0;
-// var offsetY = 0;
+var offsetY = 0;
 var intervalID = null;
 
 window.addEventListener('keydown', function (event) {
@@ -30,9 +30,13 @@ window.addEventListener('keydown', function (event) {
 });
 
 function moveCar() {
-  if ($car.className === 'east race-car') {
+  if (data.direction === 'east') {
     offsetX += 5;
+    $car.style.left = offsetX + 'px';
+  } else if (data.direction === 'west') {
+    offsetX -= 5;
     $car.style.left = offsetX + 'px';
   }
   data.xCoord = offsetX;
+  data.yCoord = offsetY;
 }
